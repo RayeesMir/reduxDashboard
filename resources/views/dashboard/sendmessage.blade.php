445c37decbc353fe077fca7b8be43fe61c79bcc1
Kumar DIvya Rajat <rajatady@users.noreply.github.com>
2015-12-07 10:39:51 +0530

    Mandrill API Working

--- resources/views/dashboard/sendmessage.blade.php
+++ resources/views/dashboard/sendmessage.blade.php
@@ -9,21 +9,18 @@
 <div class="row">
             <div class="col-lg-12">
                 <div class="panel panel-default">
-                <div class="panel-heading">Send Emails</div>
+                <div class="panel-heading">Send Message</div>
                 <div class="panel-body">
                             <div class="col-lg-8">
                                 <div class="row">
                                     <div class="col-xs-12">
-                                       <div class="input-group email">
-                                         <label>Subject of the Email</label>
-                                         <textarea class="form-control" rows="3"></textarea>
-                                        </div><!--Subject-->
+                                       
                                         <div class="input-group email">
-                                          <label>Body of the Email</label>
+                                          <label>Message to be sent</label>
                                           <textarea class="form-control" rows="5"></textarea>
                                         </div><!--Body-->
                                         <div class="input-group-btn">
-                                          <button class="btn btn-success btn-md" id="email-send">Send Mail</button>
+                                          <button class="btn btn-success btn-md" id="email-send">Send Message</button>
                                         </div>
                                     </div>
                                    
@@ -32,7 +29,7 @@
 
                             <div class="col-lg-4">
                                   <div class="panel panel-default">
-                                  <div class="panel-heading">Email Templates</div>
+                                  <div class="panel-heading">Message Templates</div>
                                   <div class="panel-body">
                                       
                                   </div><!--Email Panel Body-->
